23bcda6dac6b200df3a8a33a5ad25c9e4a78bd4c
meelhk2001 <35377632+meelhk2001@users.noreply.github.com>
2020-06-29 09:46:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat Demo',
+      title: 'Alphabics',
       theme: ThemeData(
         primaryColor: Colors.teal,
         accentColor: Colors.teal,
